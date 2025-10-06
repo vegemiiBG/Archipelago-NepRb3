5965240cf98588a45e540d693a6c3e3c8dc3dae2
vegemiiBG <twitchvegemii@gmail.com>
2025-10-06 13:28:41 -0400

    Implemented code to remove all gathers from the locations
    Currently the game will always generate with 0 locations, but when we add an option for gathers, we can turn them on or off.

--- worlds/nep_rb3/__init__.py
+++ worlds/nep_rb3/__init__.py
@@ -1,12 +1,17 @@
-from typing import Callable
+import logging
+import os
+import pkgutil
+import typing
+import settings
+from typing import Set, Dict, Any, Callable
 
 from BaseClasses import CollectionState, Region
 from worlds.AutoWorld import World
 
 from .items import NepRb3Item, item_data
-from .locations import NepRb3Location, location_table, location_table
+from .locations import NepRb3Location
 from .options import NepRb3Options
-from .locations import all_locations
+from .locations import all_locations, gathers, location_table
 
 class NepRb3World(World):
     """Nep."""
@@ -19,32 +24,40 @@ class NepRb3World(World):
     item_name_to_id = {name: data.code for name, data in item_data.items()}
     item_pool: list[NepRb3Item] = []
 
+    disabled_locations = Set[str]
+
     def create_item(self, name: str) -> NepRb3Item:
         return NepRb3Item(name, item_data[name].type, item_data[name].code, self.player)
 
     def create_regions(self) -> None:
+        self.disabled_locations = set()
         # Create regions.
         region = Region("Menu", self.player, self.multiworld)
+ 
         for index, location in enumerate(all_locations):
-            if "Herb" in location.name:
-                self.item_pool.append(self.create_item("Herb"))
+            self.disabled_locations.update([location.name for location in gathers])
 
-            if "Dogoo Jelly" in location.name:
-                self.item_pool.append(self.create_item("Dogoo Jelly"))
+        for index, location in enumerate(all_locations):
+            if location.name not in self.disabled_locations:
+                if "Herb" in location.name:
+                    self.item_pool.append(self.create_item("Herb"))
 
-            if "Coin Fragment" in location.name:
-                self.item_pool.append(self.create_item("Coin Fragment"))
+                if "Dogoo Jelly" in location.name:
+                    self.item_pool.append(self.create_item("Dogoo Jelly"))
 
-            if "Sunflowery Seed" in location.name:
-                self.item_pool.append(self.create_item("Sunflowery Seed"))
+                if "Coin Fragment" in location.name:
+                    self.item_pool.append(self.create_item("Coin Fragment"))
 
-            if "Yellow Petal" in location.name:
-                self.item_pool.append(self.create_item("Yellow Petal"))
+                if "Sunflowery Seed" in location.name:
+                    self.item_pool.append(self.create_item("Sunflowery Seed"))
 
-            if "Lizard Scale" in location.name:
-                self.item_pool.append(self.create_item("Lizard Scale"))
+                if "Yellow Petal" in location.name:
+                    self.item_pool.append(self.create_item("Yellow Petal"))
 
-            region.add_locations({location.name: location.id}, NepRb3Location)
+                if "Lizard Scale" in location.name:
+                    self.item_pool.append(self.create_item("Lizard Scale"))
+
+                region.add_locations({location.name: location.id}, NepRb3Location)
 
         self.multiworld.itempool += self.item_pool
         self.multiworld.regions.append(region)
--- worlds/nep_rb3/locations.py
+++ worlds/nep_rb3/locations.py
@@ -7,11 +7,11 @@ ap_location_base_id = 696969
 class NepRb3Location(Location):
     game = "Hyperdimension Neptunia Re;Birth3 V GENERATION"
     
-all_locations: typing.List[LocationData] = (
+gathers: typing.List[LocationData] = (
 VirtuaForestSafeZone
 )
-location_table: dict[str, int] = {
-    # fmt: off
-    "The Button":    69696969,
-    # fmt: on
-}
+all_locations: typing.List[LocationData] = (
+    gathers
+)
+
+location_table: typing.Dict[str, int] = {location.name: location.id for location in all_locations}
