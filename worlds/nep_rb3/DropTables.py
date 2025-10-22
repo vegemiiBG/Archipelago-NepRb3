0025e3c2131fced28e1c57f6d9bcf5072e65d26a
vegemiiBG <twitchvegemii@gmail.com>
2025-10-22 13:26:42 -0400

    Implemented an InternalItem class for defining the in-game contents of treasures, gathers, etc
    Renamed rb3.py to InternalItemIDs.py for more clarity, and created DropTables.py to house the in-game contents of AP items

---
+++ worlds/nep_rb3/DropTables.py
@@ -0,0 +1,51 @@
+from typing import List, Tuple
+from .names import InternalItemIDs as IDs
+
+class InternalItem:
+    itemID: int
+    amount: int
+
+vfsz_treasure1 = [InternalItem(IDs.HEALING_GRASS, 2)]
+vfsz_treasure2 = [InternalItem(IDs.HEALING_GRASS, 1)]
+vfsz_treasure3 = [InternalItem(IDs.CHIPS_PREORDERCAMPAIGN, 4)]
+vfsz_treasure4 = [InternalItem(IDs.FRAGMENT_LIFE, 1)]
+
+stationArea_treasure1 = [InternalItem(IDs.HEALING_GRASS, 2)]
+stationArea_treasure2 = [InternalItem(IDs.FRAGMENT_LIFE, 1)]
+stationArea_treasure3 = [InternalItem(IDs.ARMOR_LEATHERARMLET, 1)]
+stationArea_treasure4 = [InternalItem(IDs.BOOSTER_STR, 1)]
+
+zeca1_treasure1 = [InternalItem(IDs.PLAN_TACHI, 1)]
+zeca1_treasure2 = [InternalItem(IDs.PLAN_NEPBULL, 1)]
+zeca1_treasure3 = [InternalItem(IDs.HEALING_GRASS, 2)]
+zeca1_treasure4 = [InternalItem(IDs.FRAGMENT_LIFE, 1)]
+zeca1_treasure5 = [InternalItem(IDs.HEALING_GRASS, 3), InternalItem(IDs.PLAN_CHANGEWORLDBGM, 1)]
+zeca1_treasure6 = [InternalItem(IDs.FRAGMENT_LIFE, 1)]
+
+jetSetRange1_treasure1 = [InternalItem(IDs.PLAN_YOUTHBANDAGE, 1), InternalItem(IDs.PLAN_THESOULOFEVIL, 1)]
+jetSetRange1_treasure2 = [InternalItem(IDs.PLAN_PARALAXIN, 1), InternalItem(IDs. PLAN_GETTHEEPAINTNG, 1)]
+jetSetRange1_treasure3 = [InternalItem(IDs.HEALING_GRASS, 2)]
+jetSetRange1_treasure4 = [InternalItem(IDs.FRAGMENT_LIFE, 1), InternalItem(IDs.BOOSTER_STR, 1)]
+
+jetSetRange2_treasure1 = [InternalItem(IDs.PARALAXIN, 3), InternalItem(IDs.PLAN_HAIRANDFASHION, 1)]
+jetSetRange2_treasure2 = [InternalItem(IDs.CHIPS_SPORTS, 4)]
+jetSetRange2_treasure3 = [InternalItem(IDs.BOOSTER_INT, 1)]
+jetSetRange2_treasure4 = [InternalItem(IDs.CHIPS_FREESCENARIO, 4)]
+jetSetRange2_treasure5 = [InternalItem(IDs.FRAGMENT_LIFE, 1)]
+
+wanderersCave_treasure1 = [InternalItem(IDs.PLAN_DOGOODUDE, 1), InternalItem(IDs.PLAN_ILOVECATS, 1)]
+wanderersCave_treasure2 = [InternalItem(IDs.PLAN_HEALINGPOD, 1)]
+wanderersCave_treasure3 = [InternalItem(IDs.CHIPS_RPG, 4)]
+wanderersCave_treasure4 = [InternalItem(IDs.FRAGMENT_LIFE, 2)]
+wanderersCave_treasure5 = [InternalItem(IDs.BOOSTER_STR, 1), InternalItem(IDs.ARMOR_STEELARMLET, 1)]
+
+wanderersCaveDepths_treasure1 = [InternalItem(IDs.BOOSTER_AGI, 1), InternalItem(IDs.PLAN_THECELEB, 1)]
+wanderersCaveDepths_treasure2 = [InternalItem(IDs.SPCHARGER, 1)]
+wanderersCaveDepths_treasure3 = [InternalItem(IDs.CHIPS_WAHAHAHAHA, 4)]
+wanderersCaveDepths_treasure4 = [InternalItem(IDs.HEALING_POD, 2)]
+
+bandicrash_treasure1 = [InternalItem(IDs.PLAN_CLAYMORE, 1), InternalItem(IDs.PLAN_HATSOFTHEWORLD, 1)]
+bandicrash_treasure2 = [InternalItem(IDs.BOOSTER_STR, 3)]
+bandicrash_treasure3 = [InternalItem(IDs.CHIPS_DIESOFAST, 4)]
+bandicrash_treasure4 = [InternalItem(IDs.CHIPS_ORTHODOX, 4)]
+bandicrash_treasure5 = [InternalItem(IDs.CHIPS_NONRPGGROWTH, 4)]
