e5dd528a04b2b6ffa2db3d1345609c4497108d33
Veloalopus <joshuarhawkins1@gmail.com>
2026-05-05 18:43:27 -0700

    Up to date main folder changes

--- worlds/nep_rb3/LocationData.py
+++ worlds/nep_rb3/LocationData.py
@@ -4,8 +4,11 @@ enemy_base_id = 2000000
 
 class LocationData:
     def __init__(self,region, name, id_, itemType):
+        if itemType == 0:
+            itemType = "Gather"
         self.region = region
         self.name = f"{region} - {name}"
+        self.objectiven_name = name
         self.itemType = itemType
         self.id = id_
 
