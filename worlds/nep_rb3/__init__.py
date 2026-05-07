49f46d6f4f434bdffdca7f197132a5aac2bb68db
vegemiiBG <twitchvegemii@gmail.com>
2026-05-07 17:47:35 -0400

    im gonna find you, me
    the init.py got replaced with location_names so EVERYTHING BROKE

--- worlds/nep_rb3/__init__.py
+++ worlds/nep_rb3/__init__.py
@@ -1,282 +1,126 @@
+import logging
+import os
+import pkgutil
 import typing
-from ..LocationData import LocationData
+import settings
+from .items import dungeonItemList, filler_items, useful_items,characterItemList
+from typing import Set, Dict, Any, Callable, Optional
 
-from .adaldik_forest import *
-from .anonydeaths_lab import *
-from .anonydeaths_lab_depths import *
-from .ario_plateau import *
-from .bandicrash import *
-from .castle_chambers import *
-from .city_center import *
-from .digital_future_land import *
-from .digital_future_depths import *
-from .do_temple import *
-from .duo_r_ruins import *
-from .em_es_magma_cave import *
-from .em_es_magma_cave_depths import *
-from .extradimensional_space import *
-from .gigo_depths import *
-from .gigo_main_entrance import *
-from .graphic_pass import *
-from .graphic_pass_peak import *
-from .halo_forest import *
-from .haneda_mountain_range import *
-from .haneda_mountain_peak import *
-from .jet_set_range import *
-from .jet_set_peak import *
-from .keraga_dimension import *
-from .koagura_plateau import *
-from .kobaba_ruins import *
-from .kuzarat_facility_1 import *
-from .kuzarat_facility_2 import *
-from .lowee_castle_exterior import *
-from .lowee_castle_interior import *
-from .lowee_castle_northern_space import *
-from .lowee_castle_southern_space import *
-from .luji_plateau import *
-from .magma_cave import *
-from .magma_cave_depths import *
-from .metroid_shelter import *
-from .metroid_shelter_depths import *
-from .mines import *
-from .national_factory import *
-from .nekutoki_forest import *
-from .otori_cave import *
-from .otori_forest import *
-from .pii_shii_game_factory import *
-from .planeptune_alley import *
-from .powerlevel_island import *
-from .powerlevel_island_interior import *
-from .ps_dimension import *
-from .reload_grasslands import *
-from .rud_arms_sewer_n import *
-from .rud_arms_sewer_s import *
-from .so_shal_forest import *
-from .soni_wetlands import *
-from .station_area import *
-from .suaho_mountain_range import *
-from .suaho_mountain_peak import *
-from .under_inverse import *
-from .under_inverse_depths import *
-from .underground_cave import *
-from .vida_dimension import *
-from .virtua_forest import *
-from .virtua_forest_depths import *
-from .virtua_forest_safe_zone import *
-from .wanderers_cave import *
-from .wanderers_cave_depths import *
-from .zeca_ruins_no1 import *
-from .zeca_ruins_no2 import *
-from .zega_forest import *
+from BaseClasses import CollectionState, Region
+from worlds.AutoWorld import World
+from worlds.LauncherComponents import Component, Type, components, launch_subprocess
 
-gathers: typing.List[LocationData] = (
-    AdaldikForest,
-    AnonydeathsLab,
-    AnonydeathsLabDepths,
-    ArioPlateau,
-    Bandicrash,
-    CastleChambers,
-    CityCenter,
-    DigitalFutureLand,
-    DigitalFutureDepths,
-    DoTemple,
-    DuoRRuins,
-    EmEsMagmaCave,
-    EmEsMagmaCaveDepths,
-    ExtradimensionalSpace,
-    GigoMainEntrance,
-    GigoDepths,
-    GraphicPass,
-    GraphicPassPeak,
-    HaloForest,
-    HanedaMountainRange,
-    HanedaMountainPeak,
-    JetSetRange,
-    JetSetPeak,
-    KeragaDimension,
-    KoaguraPlateau,
-    KobabaRuins,
-    KuzaratFacility1,
-    KuzaratFacility2,
-    LoweeCastleExterior,
-    LoweeCastleInterior,
-    LoweeCastleNorthernSpace,
-    LoweeCastleSouthernSpace,
-    LujiPlateau,
-    MagmaCave,
-    MagmaCaveDepths,
-    MetroidShelter,
-    MetroidShelterDepths,
-    Mines,
-    NationalFactory,
-    NekutokiForest,
-    OtoriCave,
-    OtoriForest,
-    PiiShiiGameFactory,
-    PlaneptuneAlley,
-    PowerlevelIsland,
-    PowerlevelIslandInterior,
-    PSDimension,
-    ReloadGrasslands,
-    RudArmsSewerN,
-    RudArmsSewerS,
-    SoShalForest,
-    SoniWetlands,
-    StationArea,
-    SuahoMountainRange,
-    SuahoMountainPeak,
-    UnderInverse,
-    UnderInverseDepths,
-    UndergroundCave,
-    VidaDimension,
-    VirtuaForest,
-    VirtuaForestDepths,
-    VirtuaForestSafeZone,
-    WanderersCave,
-    WanderersCaveDepths,
-    ZecaRuinsNo1,
-    ZecaRuinsNo2,
-    ZegaForest,
-)
+def launch_client():
+    """Launch a Rb3 client"""
+    from .client import launch
+    launch_subprocess(launch, name="NepRb3Client")
 
-treasures: typing.List[LocationData] = (
-    AdaldikForestTreasures,
-    AnonydeathsLabTreasures,
-    AnonydeathsLabDepthsTreasures,
-    ArioPlateauTreasures,
-    BandicrashTreasures,
-    CastleChambersTreasures,
-    CityCenterTreasures,
-    DigitalFutureLandTreasures,
-    DigitalFutureDepthsTreasures,
-    DoTempleTreasures,
-    DuoRRuinsTreasures,
-    EmEsMagmaCaveTreasures,
-    EmEsMagmaCaveDepthsTreasures,
-    ExtradimensionalSpaceTreasures,
-    GigoMainEntranceTreasures,
-    GigoDepthsTreasures,
-    GraphicPassTreasures,
-    GraphicPassPeakTreasures,
-    HaloForestTreasures,
-    HanedaMountainRangeTreasures,
-    HanedaMountainPeakTreasures,
-    JetSetRangeTreasures,
-    JetSetPeakTreasures,
-    KeragaDimensionTreasures,
-    KoaguraPlateauTreasures,
-    KobabaRuinsTreasures,
-    KuzaratFacility1Treasures,
-    KuzaratFacility2Treasures,
-    LoweeCastleExteriorTreasures,
-    LoweeCastleInteriorTreasures,
-    LoweeCastleNorthernSpaceTreasures,
-    LoweeCastleSouthernSpaceTreasures,
-    LujiPlateauTreasures,
-    MagmaCaveTreasures,
-    MagmaCaveDepthsTreasures,
-    MetroidShelterTreasures,
-    MetroidShelterDepthsTreasures,
-    MinesTreasures,
-    NationalFactoryTreasures,
-    NekutokiForestTreasures,
-    OtoriCaveTreasures,
-    OtoriForestTreasures,
-    PiiShiiGameFactoryTreasures,
-    PlaneptuneAlleyTreasures,
-    PowerlevelIslandTreasures,
-    PowerlevelIslandInteriorTreasures,
-    PSDimensionTreasures,
-    ReloadGrasslandsTreasures,
-    RudArmsSewerNTreasures,
-    RudArmsSewerSTreasures,
-    SoShalForestTreasures,
-    SoniWetlandsTreasures,
-    StationAreaTreasures,
-    SuahoMountainRangeTreasures,
-    SuahoMountainPeakTreasures,
-    UnderInverseTreasures,
-    UnderInverseDepthsTreasures,
-    UndergroundCaveTreasures,
-    VidaDimensionTreasures,
-    VirtuaForestTreasures,
-    VirtuaForestDepthsTreasures,
-    VirtuaForestSafeZoneTreasures,
-    WanderersCaveTreasures,
-    WanderersCaveDepthsTreasures,
-    ZecaRuinsNo1Treasures,
-    ZecaRuinsNo2Treasures,
-    ZegaForestTreasures,
-)
+components.append(Component(
+    "Hyperdimension Neptunia Re;Birth3 V GENERATION Client",
+    "NepRb3Client",
+    func=launch_client,
+    component_type=Type.CLIENT
+))
 
-goalLocation: typing.List[LocationData] = (
-    CityCenterGoal,
-)
+from .items import NepRb3Item, item_data, allItemData,apCharacterItemBaseID
+from .locations import NepRb3Location
+from .options import NepRb3Options
+from .locations import all_locations, gathers, location_table
+from .names import ItemNames,progressiveGear
+from .Regions import Nep3RegionDef
+from .Rules import *
 
-levels: typing.List[LocationData] = (
-LocationData("Adaldik Forest","Grinding",40,"Level"),
-LocationData("Anonydeath's Lab Depths","Grinding",60,"Level"),
-LocationData("Anonydeath's Lab","Grinding",60,"Level"),
-LocationData("Ario Plateau","Grinding",60,"Level"),
-LocationData("Bandicrash","Grinding",20,"Level"),
-LocationData("Castle Chambers","Grinding",30,"Level"),
-LocationData("City Center","Grinding",70,"Level"),
-LocationData("Do Temple","Grinding",100,"Level"),
-LocationData("Duo R Ruins","Grinding",90,"Level"),
-LocationData("EM ES Magma Cave Depths","Grinding",80,"Level"),
-LocationData("EM ES Magma Cave","Grinding",80,"Level"),
-LocationData("Extradimensional Space","Grinding",70,"Level"),
-LocationData("Gigo Depths","Grinding",40,"Level"),
-LocationData("Gigo Main Entrance","Grinding",40,"Level"),
-LocationData("Graphic Pass Peak","Grinding",70,"Level"),
-LocationData("Graphic Pass","Grinding",70,"Level"),
-LocationData("Halo Forest","Grinding",40,"Level"),
-LocationData("Haneda Mountain Peak","Grinding",60,"Level"),
-LocationData("Haneda Mountain Range","Grinding",60,"Level"),
-LocationData("Jet Set Peak","Grinding",10,"Level"),
-LocationData("Jet Set Range","Grinding",10,"Level"),
-LocationData("Keraga Dimension","Grinding",60,"Level"),
-LocationData("Koagura Plateau","Grinding",100,"Level"),
-LocationData("Kobaba Ruins","Grinding",60,"Level"),
-LocationData("Kuzarat Facility 1","Grinding",20,"Level"),
-LocationData("Kuzarat Facility 2","Grinding",20,"Level"),
-LocationData("Lowee Castle Exterior","Grinding",30,"Level"),
-LocationData("Lowee Castle Interior","Grinding",30,"Level"),
-LocationData("Lowee Castle Northern Space","Grinding",80,"Level"),
-LocationData("Lowee Castle Southern Space","Grinding",80,"Level"),
-LocationData("Luji Plateau","Grinding",60,"Level"),
-LocationData("Magma Cave Depths","Grinding",60,"Level"),
-LocationData("Magma Cave","Grinding",60,"Level"),
-LocationData("Metroid Shelter","Grinding",50,"Level"),
-LocationData("Metroid Shelter Depths","Grinding",50,"Level"),
-LocationData("Mines","Grinding",50,"Level"),
-LocationData("National Factory","Grinding",50,"Level"),
-LocationData("Nekutoki Forest","Grinding",60,"Level"),
-LocationData("Otori Cave","Grinding",60,"Level"),
-LocationData("Otori Forest","Grinding",10,"Level"),
-LocationData("Pii Shii Game Factory","Grinding",70,"Level"),
-LocationData("Planeptune Alley","Grinding",100,"Level"),
-LocationData("Powerlevel Island Interior","Grinding",70,"Level"),
-LocationData("Powerlevel Island","Grinding",70,"Level"),
-LocationData("PS Dimension","Grinding",50,"Level"),
-LocationData("Reload Grasslands","Grinding",30,"Level"),
-LocationData("Rud Arms Sewer N.","Grinding",30,"Level"),
-LocationData("Rud Arms Sewer S.","Grinding",30,"Level"),
-LocationData("So Shal Forest","Grinding",80,"Level"),
-LocationData("Soni Wetlands","Grinding",30,"Level"),
-LocationData("Station Area","Grinding",10,"Level"),
-LocationData("Suaho Mountain Range","Grinding",40,"Level"),
-LocationData("Suaho Mountain Peak","Grinding",40,"Level"),
-LocationData("Under Inverse","Grinding",100,"Level"),
-LocationData("Under Inverse Depths","Grinding",100,"Level"),
-LocationData("Underground Cave","Grinding",30,"Level"),
-LocationData("Vida Dimension","Grinding",80,"Level"),
-LocationData("Virtua Forest Depths","Grinding",70,"Level"),
-LocationData("Virtua Forest Safe Zone","Grinding",10,"Level"),
-LocationData("Virtua Forest","Grinding",70,"Level"),
-LocationData("Wanderer's Cave","Grinding",20,"Level"),
-LocationData("Wanderer's Cave Depths","Grinding",20,"Level"),
-LocationData("Zeca Ruins No.1","Grinding",10,"Level"),
-LocationData("Zeca Ruins No.2","Grinding",20,"Level"),
-LocationData("Zega Forest","Grinding",50,"Level"),
-)
+class NepRb3World(World):
+    """Nep."""
+
+    game = "Hyperdimension Neptunia Re;Birth3 V GENERATION"
+    options: NepRb3Options
+    options_dataclass = NepRb3Options
+    location_name_to_id = {loc_data.name: loc_data.id for loc_data in all_locations}
+
+    item_name_to_id = {name: data.code for name, data in allItemData.items()}
+    item_pool: list[NepRb3Item] = []
+
+    disabled_locations = Set[str]
+
+    def create_item(self, name: str) -> NepRb3Item:
+        return NepRb3Item(name, allItemData[name].type, allItemData[name].code, self.player)
+
+    def create_regions(self) -> None:
+        self.disabled_locations = set()
+        # Create regions.
+        devin = Nep3RegionDef(self.multiworld,self.player,self.options)
+        devin.setup_region_and_locations()
+        devin.create_dungeon_exits()
+        set_all_planeptune_dungeons(self)
+        set_all_lastation_dungeons(self)
+        set_all_lowee_dungeons(self)
+        set_all_leanbox_dungeons(self)
+        set_all_hyper_dungeons(self)
+        set_all_hello_dungeons(self)
+        set_all_pc_dungeons(self)
+        set_all_eden_dungeons(self)
+        set_win_condition(self)
+
+
+    def create_items(self) -> None:
+        item_pool= []
+        item_pool.append(self.create_item(ItemNames.neps_pudding))
+        item_pool.append(self.create_item(ItemNames.compas_syringe))
+        item_pool.append(self.create_item(ItemNames.ifs_notebook))
+        item_pool.append(self.create_item(ItemNames.plutia_doll))
+        item_pool.append(self.create_item(ItemNames.peashys_drawing))
+        for DungeonName in dungeonItemList.keys():
+            item_pool.append(self.create_item(DungeonName))
+        # Starting Character
+        starting_character = self.random.choice(list(characterItemList.keys()))
+        self.multiworld.push_precollected(self.create_item(starting_character))
+        self.starting_character = characterItemList[starting_character].code - apCharacterItemBaseID
+
+        for CharacterName in characterItemList.keys():
+            if starting_character == CharacterName: continue
+            item_pool.append(self.create_item(CharacterName))
+            
+        for i in range(0,6):
+            item_pool.append(self.create_item(progressiveGear.neptune_progressive_gear))
+        for i in range(0,6):
+            item_pool.append(self.create_item(progressiveGear.noire_progressive_gear))
+        for i in range(0,6):
+            item_pool.append(self.create_item(progressiveGear.plutia_progressive_gear))
+        for i in range(0,5):
+            item_pool.append(self.create_item(progressiveGear.blanc_progressive_gear))
+        for i in range(0,5):
+            item_pool.append(self.create_item(progressiveGear.vert_progressive_gear))
+        for i in range(0,5):
+            item_pool.append(self.create_item(progressiveGear.nepgear_progressive_gear))
+        for i in range(0,5):
+            item_pool.append(self.create_item(progressiveGear.peashy_progressive_gear))
+        for i in range(0,3):
+            item_pool.append(self.create_item(progressiveGear.uni_progressive_gear))
+        for i in range(0,4):
+            item_pool.append(self.create_item(progressiveGear.rom_progressive_gear))
+        for i in range(0,4):
+            item_pool.append(self.create_item(progressiveGear.ram_progressive_gear))
+        for i in range(0,6):
+            item_pool.append(self.create_item(progressiveGear.progressive_armor))
+
+        numbersOfItemsInTheGame = len(self.multiworld.get_unfilled_locations(self.player))
+        while numbersOfItemsInTheGame > len(item_pool):
+            if self.random.randrange(0,100) > 55:
+                item_pool.append(self.create_item(useful_items[self.random.randrange(0,len(useful_items))]))
+            else:
+                item_pool.append(self.create_item(filler_items[self.random.randrange(0,len(filler_items))]))
+        self.multiworld.itempool += item_pool
+    
+    def get_filler_item_name(self) -> str:
+        return
+
+
+    def set_rules(self) -> None:
+        
+        return
+
+    def fill_slot_data(self) -> dict:
+        return {
+            "start_character":self.starting_character
+        }
