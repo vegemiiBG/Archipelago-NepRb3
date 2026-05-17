a8ce0d2e9bc71d79f1e89fbc458ca45f846176c2
vegemiiBG <twitchvegemii@gmail.com>
2026-05-16 23:18:51 -0400

    Fixed Kuzarat Facility's Healing Dogoo

--- worlds/nep_rb3/location_names/kuzarat_facility_2.py
+++ worlds/nep_rb3/location_names/kuzarat_facility_2.py
@@ -23,6 +23,6 @@ KuzaratFacility2Enemies: typing.List[LocationData] = (
 LocationData("Kuzarat Facility 2","Matango", 265, "Enemy"),
 LocationData("Kuzarat Facility 2","R-4", 139, "Enemy"),
 LocationData("Kuzarat Facility 2","Created Soldier", 291, "Enemy"),
-LocationData("Kuzarat Facility 1","Healing Dogoo", 106, "Enemy"),
+LocationData("Kuzarat Facility 2","Healing Dogoo", 106, "Enemy"),
 LocationData("Kuzarat Facility 2","R4i-SDHC", 419, "Big Enemy"),
 )
